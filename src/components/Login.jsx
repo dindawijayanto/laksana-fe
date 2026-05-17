9c31b7fe4a82e8c30b4e8ca6f282e522334992c7
dindawijayanto <dindawijayantoo@gmail.com>
2026-05-18 02:15:53 +0700

    ini akhir bgt

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     setError('');
 
     try {
-      const response = await api.post('/login', {
+      const response = await api.post('/api/login', {
         email,
         password
       });
