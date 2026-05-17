dfa9a33860d4480049782f27033118c681df3f35
dindawijayanto <dindawijayantoo@gmail.com>
2026-05-18 00:55:32 +0700

    fix: hardcode API URL

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from './api';
+import api from '../api';
 import { useNavigate } from 'react-router-dom';
 
 export default function AdminDashboard() {
